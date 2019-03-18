1655749db07f369ce62a85f456d63dbb800ed47d
yurdoos <48571395+yurdoos@users.noreply.github.com>
2019-03-18 20:45:59 +0400

    Update amocrmtest.php

--- amocrmtest.php
+++ amocrmtest.php
@@ -1,3 +1,5 @@
+<?php
+error_reporting(0);
 if($_GET['cmd']=="newlead"){
 
 $id_lead = $_POST['leads']['add'][0]['id'];
@@ -12,6 +14,51 @@ $subdomain=test9956567;
 
 $link='https://test9956567.amocrm.ru/private/api/auth.php?type=json';
 
+$curl=curl_init(); 
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
+curl_setopt($curl,CURLOPT_URL,$link);
+curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
+curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($user));
+curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+curl_setopt($curl,CURLOPT_HEADER,false);
+curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+$out=curl_exec($curl);
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE); 
+curl_close($curl); 
+/* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+$code=(int)$code;
+$errors=array(
+  301=>'Moved permanently',
+  400=>'Bad request',
+  401=>'Unauthorized',
+  403=>'Forbidden',
+  404=>'Not found',
+  500=>'Internal server error',
+  502=>'Bad gateway',
+  503=>'Service unavailable'
+);
+try
+{
+  #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+ if($code!=200 && $code!=204)
+    throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error',$code);
+}
+catch(Exception $E)
+{
+  die('Ошибка: '.$E->getMessage().PHP_EOL.'Код ошибки: '.$E->getCode());
+}
+
+$Response=json_decode($out,true);
+$Response=$Response['response'];
+if(isset($Response['auth'])) 
+ return 'Авторизация прошла успешно';
+return 'Авторизация не удалась';
+
+amocrm_updateLead($id_lead,$name_lead);
 
 function amocrm_updateLead($id,$name){
 
@@ -77,6 +124,6 @@ $Response=$Response['response']; #Response - объект класса StdClass
 return $Response;
 }
 
-amocrm_updateLead($id_lead,$name_lead);
+
 exit;
 }
