26a9ea49ce9b9751686005c381812030f73230f6
yurdoos <48571395+yurdoos@users.noreply.github.com>
2019-03-18 17:45:02 +0400

    Update amocrmtest.php

--- amocrmtest.php
+++ amocrmtest.php
@@ -12,23 +12,6 @@ $subdomain=test9956567;
 
 $link='https://test9956567.amocrm.ru/private/api/auth.php?type=json';
 
-$curl=curl_init();
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-API-client/1.0');
-curl_setopt($curl,CURLOPT_URL,$link);
-curl_setopt($curl,CURLOPT_CUSTOMREQUEST,'POST');
-curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($user));
-curl_setopt($curl,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-curl_setopt($curl,CURLOPT_HEADER,false);
-curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__).'/cookie.txt');
-curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__).'/cookie.txt');
-curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
-$out=curl_exec($curl);
-$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
-curl_close($curl); 
-
-
 
 function amocrm_updateLead($id,$name){
 
@@ -40,7 +23,7 @@ $leads['request']['leads']['update']=array(
     'last_modified'=>time(),
  )
  );
- $link='https://'.ecoyuriy.'.amocrm.ru/private/api/v2/json/leads/set';
+ $link='https://test9956567.amocrm.ru/private/api/auth.php?type=json';
  $result = amocrm_Request($link,$leads);
 
 }
